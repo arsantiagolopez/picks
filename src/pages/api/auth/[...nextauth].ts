b9a57008a9d43b4ffe1a5e8a6306f3755bd1d53a
Alexander Santiago <alexandersantiago@users-MacBook-Air.local>
2022-04-07 12:00:58 -0500

    Update session max age.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -29,10 +29,10 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     secret: process.env.SECRET,
     session: {
       // Seconds - How long until an idle session expires and is no longer valid.
-      maxAge: 60 * 60 * 24 * 30, // 1 month
+      maxAge: 60 * 60 * 24 * 30 * 365, // 1 month
       // Seconds - Throttle how frequently to write to database to extend a session.
       // Use it to limit write operations. Set to 0 to always update the database.
-      updateAge: 60 * 60 * 24 * 30, // 1 month
+      updateAge: 60 * 60 * 24 * 30 * 365, // 1 month
     },
     pages: {
       signIn: "/signin",
--- src/puppeteer/scrapeReddit.ts
+++ src/puppeteer/scrapeReddit.ts
@@ -1,10 +1,8 @@
-import puppeteer from "puppeteer";
-
 // Scrape reddit comments from the r/sportsbook tennis threads
 const scrapeReddit = async () => {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
   } catch (err) {
     console.log(err);
   }
